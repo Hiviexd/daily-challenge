e286778209717ed1a2142d775545c759693e6dfb
Hivie <me@hivie.tn>
2025-05-20 22:34:01 +0100

    fix settings config not being created properly

--- server/models/settingsModel.ts
+++ server/models/settingsModel.ts
@@ -7,10 +7,10 @@ const SettingsSchema = new mongoose.Schema({
 
 SettingsSchema.statics.getSettingsConfig = async function () {
     // There can only be one settings object, if it's missing, create a new one
-    const settings = await this.findOne({});
+    let settings = await this.findOne({});
     if (!settings) {
         console.error("No settings config found in database, creating a new one");
-        return this.create({});
+        settings = await this.create({ mods: {} });
     }
     return settings;
 };
